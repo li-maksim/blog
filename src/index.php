4f9b46f93b604279420274fee944d47cfeeb5cbc
Maksim <maksim.li3018@gmail.com>
2026-01-28 19:24:27 +0300

    Established database connection

--- src/index.php
+++ src/index.php
@@ -1,3 +1,39 @@
+<?php 
+
+$config = [
+    'host'     => getenv('DB_HOST'),
+    'user'     => getenv('DB_USER'),
+    'pass'     => getenv('DB_PASS'),
+    'database' => getenv('DB_DATABASE'),
+    'driver'   => getenv('DB_DRIVER'),
+];
+
+$pdo = new PDO(
+    $config['driver'] . ':host=' . $config['host'] . ';dbname=' . $config['database'],
+    $config['user'],
+    $config['pass'],
+    [
+        PDO::ATTR_EMULATE_PREPARES   => false,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ]
+);
+
+$stmt = $pdo->query(
+    'SELECT
+        title, created_at, body
+    FROM
+        post
+    ORDER BY
+        created_at DESC'
+);
+
+if ($stmt === false)
+{
+    throw new Exception('There was a problem running this query');
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -13,6 +49,7 @@
         <p>
             <a href="#">Read more...</a>
         </p>
+        <?php echo $stmt->fetch()['title'] ?>
         <h2>Article 2 title</h2>
         <div>dd Mon YYYY</div>
         <p>A paragraph summarising article 2.</p>
